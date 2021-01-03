79a66995a068552aff128cb16e867664d4a5a89a
mrkaye97 <mrkaye97@gmail.com>
2021-01-03 13:22:44 -0500

    deploy with keys

--- R/mod_catch_prob.R
+++ R/mod_catch_prob.R
@@ -8,12 +8,6 @@
 #'
 #' @importFrom shiny NS tagList
 `%>%` <- magrittr::`%>%`
-load('model_data/sodium_key.Rdata')
-key <- cyphr::key_sodium(k)
-
-load_encrypted <- function(file, key) {
-  cyphr::decrypt(load(file, envir = globalenv()), key)
-}
 
 mod_catch_prob_ui <- function(id){
   ns <- NS(id)
@@ -39,8 +33,13 @@ mod_catch_prob_ui <- function(id){
                                   choices = c('Target Model', 'Catch Model'),
                                   multiple = TRUE,
                                   options = shinyWidgets::pickerOptions(maxOptions = 1)),
+        shinyWidgets::pickerInput(NS(id, "outtype"),
+                                  label = "Output",
+                                  choices = c('Rankings', 'Features'),
+                                  multiple = TRUE,
+                                  options = shinyWidgets::pickerOptions(maxOptions = 1)),
         actionButton(NS(id, 'loadmodel'), label = "Load model", width = '100%', style = 'margin-bottom:10px'),
-        shinyWidgets::pickerInput(NS(id, "feats"),
+        shinyWidgets::pickerInput(NS(id, "cols_to_select"),
                                   label = "Features",
                                   choices = "Please select a model first",
                                   multiple = TRUE,
@@ -49,7 +48,7 @@ mod_catch_prob_ui <- function(id){
         actionButton(NS(id, "rendertable"), "Render Table", width = '100%', style = 'margin-bottom:10px')
       ),
       mainPanel(
-        DT::dataTableOutput(ns("featuretab"))
+        gt::gt_output(ns("featuretab"))
       )
     )
   )
@@ -66,22 +65,30 @@ mod_catch_prob_server <- function(id) {
       ns <- session$ns
 
       model_data <- reactiveValues()
+
       observeEvent(input$loadmodel, {
         tryCatch({
           if (is.na(input$mod)) {
             showNotification('You must select a model to load', duration = 5, type = 'error')
+          } else if (is.na(input$outtype)) {
+            showNotification('You must select an output type', duration = 5, type = 'error')
           }
 
-          if (input$mod == 'Target Model') {
-            load_encrypted('model_data/catch_prob_features.Rdata', key)
-            ## add more loads
+          if (input$mod == 'Target Model' & input$outtype == 'Rankings') {
+            df <- load_encrypted('model_data/catch_prob_features.Rdata')
+          } else if (input$mod == 'Target Model' & input$outtype == 'Features') {
+            df <- load_encrypted('model_data/catch_prob_features.Rdata')
+          } else if (input$mod == 'Catch Model' & input$outtype == 'Rankings') {
+            load('model_data/drops_added.Rdata')
+            df <- results
+            rm(results)
           } else {
             ## change this
-            load_encrypted('model_data/catch_prob_features.Rdata', key)
+            df <- load_encrypted('model_data/catch_prob_features.Rdata')
             ## add more loads
           }
 
-          shinyWidgets::updatePickerInput(session, 'feats', choices = sort(colnames(df)), label = "Features")
+          shinyWidgets::updatePickerInput(session, 'cols_to_select', choices = sort(colnames(df)), label = "Features")
           model_data[['features']] <- df
           ## add more model data
 
@@ -98,16 +105,16 @@ mod_catch_prob_server <- function(id) {
       rendered_table <- observeEvent(input$rendertable, {
         tryCatch({
 
-          if (length(input$feats) == 0) {
+          if (length(input$cols_to_select) == 0) {
             showNotification('You must add at least one school to your list', duration = 5, type = 'error')
           }
 
+          tab <- model_data[['features']] %>%
+            dplyr::select(input$cols_to_select) %>%
+            gt::gt() %>%
+            bdb2021::gt_theme_538()
 
-
-          tab <- df %>%
-            dplyr::select(input$feats)
-
-          output$featuretab <- DT::renderDataTable(tab)
+          output$featuretab <- gt::render_gt(tab)
         },
         error = function(err){
           message(err)
